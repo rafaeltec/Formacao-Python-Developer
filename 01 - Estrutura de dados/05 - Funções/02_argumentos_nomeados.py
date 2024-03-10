67f6e365e0bf74962660e0afe5292852c11a5b52
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2024-03-10 20:42:55 -0300

    add

--- 01 - Estrutura de dados/05 - Funções/02_argumentos_nomeados.py
+++ 01 - Estrutura de dados/05 - Funções/02_argumentos_nomeados.py
@@ -1,7 +1,17 @@
+# Definição da função 'salvar_carro'. Ela recebe quatro argumentos: marca, modelo, ano e placa.
 def salvar_carro(marca, modelo, ano, placa):
+    # Esta linha é apenas um comentário para indicar que normalmente aqui iria o código para salvar o carro no banco de dados.
+    # salva carro no banco de dados...
 
-    print(f"carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+    # Aqui, a função imprime uma mensagem formatada contendo os detalhes do carro.
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
 
-salvar_carro("fiat","palio",1999,"abc-1232")
+# Primeira chamada da função: os argumentos são passados diretamente e na ordem definida na função.
+salvar_carro("Fiat", "Palio", 1999, "ABC-1234")
 
-print(salvar_carro)
+# Segunda chamada da função: os argumentos são passados utilizando o nome dos parâmetros. Isso é útil para melhorar a legibilidade e permite passar os argumentos fora de ordem, se necessário.
+salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234")
+
+# Terceira chamada da função: os argumentos são passados usando um dicionário com a desestruturação (**). 
+# Isso é útil quando os dados já estão em um dicionário e você quer passá-los diretamente para a função.
+salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234"})
